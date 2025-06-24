b2ddcf6f3ed709db034ae974bb925efe0f050735
Ivan <ivanmalaevkirova99@gmail.com>
2025-06-24 19:46:52 +0400

    update 0.0.0.1

--- task3.5_array_and_slice/main.go
+++ task3.5_array_and_slice/main.go
@@ -9,6 +9,7 @@ func main() {
 	numbers := []int{1, 5, 7, 10, 27, 35}
 	numbers = append(numbers, 40)
 	numbers = append(numbers[:1], numbers[10+1:]...)
-	fmt.Println("Длина", len(numbers))
+	fmt.Println("В срезе", numbers)
+	fmt.Println("длина", len(numbers))
 
 }
