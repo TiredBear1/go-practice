7d40a37c36f4fee598d7f052a2d595be08dedbf0
Ivan <ivanmalaevkirova99@gmail.com>
2025-06-25 21:36:32 +0400

    Block 3.7, task 3 completed

--- task3.7_Structures_and_methods/main.go
+++ task3.7_Structures_and_methods/main.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+/* task 1-2
 type User struct {
 	Name string
 	Age  int
@@ -20,4 +21,32 @@ func main() {
 	fmt.Scan(&a)
 	person := User{Name: n, Age: a}
 	person.Greet()
+}
+*/
+
+type User struct {
+	Name string
+	Age  int
+}
+
+func (u User) Greet() {
+	fmt.Println("My name is ", u.Name, " and I'm ", u.Age, " old!")
+}
+
+func main() {
+	var name string
+	var age int
+	var people []User
+
+	for i := 0; i < 5; i++ {
+		fmt.Print("Введите Имя:")
+		fmt.Scan(&name)
+		fmt.Print("Введите Возраст:")
+		fmt.Scan(&age)
+		people = append(people, User{Name: name, Age: age})
+	}
+	for _, user := range people {
+		user.Greet()
+	}
+
 }
